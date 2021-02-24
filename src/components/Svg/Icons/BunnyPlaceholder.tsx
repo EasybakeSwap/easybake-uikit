0f11f912c573731fd84f42dec9271abe7d977f47
Uni <CryptoUnico@gmail.com>
2021-02-24 00:56:44 -0600

    images

--- src/components/Svg/Icons/BunnyPlaceholder.tsx
+++ src/components/Svg/Icons/BunnyPlaceholder.tsx
@@ -11,8 +11,6 @@ const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 34 38" {...props}>
       <circle cx="16" cy="16" r="16" fill="#F7A8B8" />
-
-      
     </Svg>
   );
 };
--- src/components/Svg/Icons/TicketRound.tsx
+++ src/components/Svg/Icons/TicketRound.tsx
@@ -5,10 +5,9 @@ import { SvgProps } from "../types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 34 38" {...props}>
-    <circle cx="16" cy="16" r="16" fill="#F7A8B8" />
-
-    
-  </Svg>  );
+      <circle cx="16" cy="16" r="16" fill="#F7A8B8" />
+    </Svg>
+  );
 };
 
 export default Icon;
