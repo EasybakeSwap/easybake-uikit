3113d848a380fd4a9efa812cc57a560748d57bea
Lucus <lucusra@protonmail.com>
2021-04-30 17:15:35 +1000

    alligned icons with price icon above

--- src/widgets/Menu/components/ThemeSwitcher.tsx
+++ src/widgets/Menu/components/ThemeSwitcher.tsx
@@ -14,16 +14,16 @@ interface Props {
 }
 
 const ThemeSwitcher: React.FC<Props> = ({ isDark, toggleTheme }) => (
-  <Button variant="text" onClick={() => toggleTheme(!isDark)}>
-    {/* alignItems center is a Safari fix */}
-    <Flex alignItems="center">
-      <SunIcon color={isDark ? "textDisabled" : "text"} width="24px" />
-      <Text color="textDisabled" mx="4px">
-        /
-      </Text>
-      <MoonIcon color={isDark ? "text" : "textDisabled"} width="24px" />
-    </Flex>
-  </Button>
+    <Button variant="text" onClick={() => toggleTheme(!isDark)}>
+      {/* alignItems center is a Safari fix */}
+      <Flex alignItems="center" position='relative' right='15px'>
+        <SunIcon color={isDark ? "textDisabled" : "text"} width="24px"/>
+        <Text color="textDisabled" mx="4px">
+          /
+        </Text>
+        <MoonIcon color={isDark ? "text" : "textDisabled"} width="24px" />
+      </Flex>
+    </Button>
 );
 
 export default React.memo(ThemeSwitcher, (prev, next) => prev.isDark === next.isDark);
