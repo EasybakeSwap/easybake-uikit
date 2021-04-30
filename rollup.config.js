24dc09ec0b4b68068a04dd8ec34bf6a4b58a5c07
Lucus <lucusra@protonmail.com>
2021-04-30 23:55:40 +1000

    config changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
 import typescript from "@rollup/plugin-typescript";
-import url from "@rollup/plugin-url";
+import pkg from "./package.json";
 
 export default {
   input: "src/index.ts",
   output: [
-    { file: main, format: "cjs" },
-    { file: module, format: "es" },
+    { file: pkg.main, format: "cjs" },
+    { file: pkg.module, format: "es" },
   ],
-  plugins: [url(), typescript()],
+  plugins: [typescript()],
 };
